dffbc4e37fdea953803bea4dc36db8c8f5a8716a
gunnerDgd <ifstudiomail@naver.com>
2022-07-28 02:26:57 +0900

    MSVC Alignement Error Fixed.

--- source/lib/system/allocation/system_allocate.c
+++ source/lib/system/allocation/system_allocate.c
@@ -1,7 +1,9 @@
 #include <memory/system/allocation/system_allocate.h>
 
 #include <stdlib.h>
+#ifndef SYNAPSE_BUILD_ENVIRONMENT_MSVC
 #include <stdalign.h>
+#endif
 
 void*
     synapse_memory_allocate_from_system
@@ -22,15 +24,26 @@ void*
     synapse_memory_aligned_allocate_from_system
         (void* pAllocHint, size_t pAllocSize, size_t pAllocAlign)
 {
+#ifdef SYNAPSE_BUILD_ENVIRONMENT_MSVC
     return 
         _aligned_malloc
             (pAllocSize, pAllocAlign);
+#else
+    return
+        aligned_alloc
+            (pAllocAlign, pAllocSize);
+#endif
 }
 
 void
     synapse_memory_aligned_deallocate_from_system
         (void* pDealloc, size_t pDeallocSize)
 {
+#ifdef SYNAPSE_BUILD_ENVIRONMENT_MSVC
     _aligned_free
         (pDealloc);
+#else
+    free
+        (pDealloc);
+#endif
 }
